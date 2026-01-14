e6b2172b136b78fc38b8bf65c0a4181e131a981f
Sahnawaz Hussain <sahnawaz643786@gmail.com>
2026-01-14 11:24:53 +0530

    Basic route setup is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Home from "./Pages/Home";
+import MainRoute from "./Routes/MainRoute";
 
 const App = () => {
   return <div>
-    <Home/>
+    <MainRoute/>
   </div>;
 };
 
